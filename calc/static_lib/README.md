b952cc0e7482061615a125fd50f70d893271e0ad
Andrey <you@example.com>
2026-03-30 23:10:08 +0700

    fix scanf

--- calc/static_lib/README.md
+++ calc/static_lib/README.md
@@ -1,1 +1,9 @@
+#Пример запуска
 
+make
+make install PREFIX=~/opt/calc
+LD_LIBRARY_PATH=~/opt/calc/lib  ~/opt/calc/bin/calc
+
+--------------------
+make
+make run
--- calc/static_lib/source/main.c
+++ calc/static_lib/source/main.c
@@ -1,19 +1,36 @@
 #include <stdio.h>
-#include <stdlib.h>
+//#include <stdlib.h>
 #include "../include/static_calc.h"
 #include <unistd.h>
 #define CLEAR_TERMINAL "\033[2J\033[H"
-#define RED "\033[31m"
+//#define RED "\033[31m"
+
+void clear_input_buffer() {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF) {
+    }
+}
 
 int main(){
 
     int op;
+    char tail;
     long res;
     int operand2, operand1;
     while(1){
         printf("Введите номер бинарной операции [1-4]\n");
         printf("1)Сложение\n2)Вычитание\n3)Умножение\n4)Деление\n5)Очистить терминал\n6)Выход\n");
-        scanf("%d", &op);
+        //scanf("%d", &op);
+        if (scanf("%d%c", &op, &tail) != 2 || tail != '\n') {
+        //if (scanf("%d", &op) != 1) {
+            printf("Ошибка ввода: нужно ввести число [1-4]\n");
+            clear_input_buffer();
+            sleep(2);
+            printf(CLEAR_TERMINAL);
+            continue;
+        }
+
+        //clear_input_buffer();
 
         if(op == 6){
             printf("Вы вышли из программы\n");
@@ -33,7 +50,17 @@ int main(){
         }
 
         printf("Введите два операнда в диапазоне [-2147483648, 2147483647]\n");
-        scanf("%d %d", &operand1, &operand2);
+
+        if (scanf("%d %d%c", &operand1, &operand2, &tail) != 3 || tail != '\n') {
+            printf("Ошибка ввода: нужно ввести два целых числа\n");
+            clear_input_buffer();
+            printf(CLEAR_TERMINAL);
+            sleep(2);
+            continue;;
+        }
+        
+        //clear_input_buffer();
+
         switch(op){
             case 1:
                 res = calc_add(operand1, operand2);
