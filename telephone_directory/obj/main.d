cb33a80c4d2e430d55b06eb97c003bdf43c8269d
Andrey <you@example.com>
2026-04-03 21:14:07 +0700

    fix .gitignore

---
+++ telephone_directory/obj/main.d
@@ -0,0 +1,2 @@
+obj/main.o: source/main.c include/telephone_directory.h
+include/telephone_directory.h:
---
+++ telephone_directory/obj/telephone_directory.d
@@ -0,0 +1,3 @@
+obj/telephone_directory.o: source/telephone_directory.c \
+ include/telephone_directory.h
+include/telephone_directory.h:
